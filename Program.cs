3be31edb1423f728c315b972e66df79dd2065121
ttheusLima <ttheus.rlima@gmail.com>
2022-10-04 10:55:16 -0300

    Criado método para descobrir a raiz quadrada de um número com a classe Math

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@
 
 Calcular calculo = new Calcular();
 
-// calculo.Somar(5, 5);
-// calculo.Subtrair(15,50);
-// calculo.Multiplicar(5,6);
-// calculo.Dividir(2,4);
-// calculo.Potencia(3,3);
+calculo.Somar(5, 5);
+calculo.Subtrair(15,50);
+calculo.Multiplicar(5,6);
+calculo.Dividir(2,4);
+calculo.Potencia(3,3);
 calculo.Seno(30);
 calculo.Coseno(30);
 calculo.Tangente(30);
+calculo.RaizQuadrada(64);
