09bb3af244ea612a87fbc1ade30e1e2326f25124
ttheusLima <ttheus.rlima@gmail.com>
2022-10-04 10:43:00 -0300

    Adicionado 4 métodos da função aritmética básica e três métodos daa funções trigométrica

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculadora.Entities;
+
+Calcular calculo = new Calcular();
+
+// calculo.Somar(5, 5);
+// calculo.Subtrair(15,50);
+// calculo.Multiplicar(5,6);
+// calculo.Dividir(2,4);
+// calculo.Potencia(3,3);
+calculo.Seno(30);
+calculo.Coseno(30);
+calculo.Tangente(30);
